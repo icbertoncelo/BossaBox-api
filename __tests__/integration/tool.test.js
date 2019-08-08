f9f5b2f8926a974df60c1c29813a2c1e7b12ff71
Ian Carlos <carlos.ian007@gmail.com>
2019-08-08 10:04:05 -0300

    Feat(tools): Add tool tests

--- __tests__/integration/tool.test.js
+++ __tests__/integration/tool.test.js
@@ -20,4 +20,37 @@ describe('Tools', () => {
 
     expect(response.body).toHaveProperty('id');
   });
+
+  it('Should be able to list all tools', async () => {
+    const user = await factory.create('User');
+    const tool = await factory.attrs('Tool');
+
+    await request(app)
+      .post('/tools')
+      .set('Authorization', `Bearer ${user.generateToken(user)}`)
+      .send(tool);
+
+    const response = await request(app)
+      .get('/tools')
+      .set('Authorization', `Bearer ${user.generateToken(user)}`);
+
+    expect(response.body.length).toBe(1);
+    expect(response.status).toBe(200);
+  });
+
+  it('Should be able to remove a tool', async () => {
+    const user = await factory.create('User');
+    const tool = await factory.attrs('Tool');
+
+    const newTool = await request(app)
+      .post('/tools')
+      .set('Authorization', `Bearer ${user.generateToken(user)}`)
+      .send(tool);
+
+    const response = await request(app)
+      .delete(`/tools/${newTool.body.id}`)
+      .set('Authorization', `Bearer ${user.generateToken(user)}`);
+
+    expect(response.statusCode).toBe(204);
+  });
 });
